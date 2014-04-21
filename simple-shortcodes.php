1ccfdc257214f8bb020b52e43abb43d95d9ddf19
Constantine Kiriaze <ckiriaze@gmail.com>
2014-04-21 09:12:14 -0700

    updated shortcodes wrapper

--- simple-shortcodes.php
+++ simple-shortcodes.php
@@ -50,7 +50,8 @@ define( 'SIMPLE_SHORTCODES_PLUGIN_BASENAME', plugin_basename(__FILE__));
 //  Wrapped in after_setup_theme to utilize options
 add_action('after_setup_theme', 'simple_shortcodes_init');
 function simple_shortcodes_init(){
-if ( is_admin() && ( ! defined( 'DOING_AJAX' ) || ! DOING_AJAX ) ) {
-    //  Load class
-    require_once( SIMPLE_SHORTCODES_PLUGIN_PATH . 'class-simple-shortcodes.php' );
+    if ( is_admin() && ( ! defined( 'DOING_AJAX' ) || ! DOING_AJAX ) ) {
+        //  Load class
+        require_once( SIMPLE_SHORTCODES_PLUGIN_PATH . 'class-simple-shortcodes.php' );
+    }
 }
